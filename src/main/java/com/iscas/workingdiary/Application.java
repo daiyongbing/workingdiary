07d0ec50c2352172237464daaadd6c0b7bb5d002
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2018-11-15 17:28:05 +0800

    配置RepChain

--- src/main/java/com/iscas/workingdiary/Application.java
+++ src/main/java/com/iscas/workingdiary/Application.java
@@ -3,11 +3,12 @@ package com.iscas.workingdiary;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.iscas.workingdiary.mapper")
-//@ComponentScan("com.iscas.workingdiary.mapper")
+//@ConfigurationProperties(prefix = "repchain")
 public class Application {
 
     public static void main(String[] args) {
