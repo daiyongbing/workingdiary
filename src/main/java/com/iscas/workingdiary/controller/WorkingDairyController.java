5d9ef6b2507c71c1df0c68edc0899b83c2fde0c6
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2018-11-15 10:53:53 +0800

    新增WorkingDiaryController

---
+++ src/main/java/com/iscas/workingdiary/controller/WorkingDairyController.java
@@ -0,0 +1,18 @@
+﻿package com.iscas.workingdiary.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/user")
+public class WorkingDairyController {
+    public void postWorkingDairyWithSign(HttpServletRequest request){
+        String userName = request.getParameter("userName");
+        String title = request.getParameter("title");
+        String text = request.getParameter("text");
+
+        
+    }
+}
